703f0b2f6721e0547920f61f0bbf3f44f2d6a37f
Stachix83 <153676755+Stachix83@users.noreply.github.com>
2025-03-07 11:49:20 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ from routes import devices, users, remote, websocket
 app = FastAPI()
 
 app.include_router(users.router, prefix="/api")
-#app.include_router(devices.router, prefix="/api")
+app.include_router(devices.router, prefix="/api")
 app.include_router(remote.router, prefix="/api")
-#app.include_router(websocket.router, prefix="/api")
+app.include_router(websocket.router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
